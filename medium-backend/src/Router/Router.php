94aa0a894ca76c982f5e7ee250ae2b568a051d1c
Arun Bohra <arunbohra12@gmail.com>
2022-12-24 14:28:55 +0530

    Add documentation for the router class

--- medium-backend/src/Router/Router.php
+++ medium-backend/src/Router/Router.php
@@ -5,9 +5,10 @@ namespace App\Router;
 use Closure;
 
 class Router {
-  private string $endpoint;
-  private string $method;
+  private string $endpoint; // Current route on which request is being made to
+  private string $method; // Request method
 
+  // This will store the different handlers for different routes and methods
   private array $routes = array(
     'GET' => array(),
     'POST' => array(),
@@ -18,6 +19,11 @@ class Router {
     $this->method = $request['REQUEST_METHOD'];
   }
 
+  /*
+   * Checks if the route is registered in $this->routes
+   * @params string $endPoint -> the route for on which request is being made
+   * @return bool -> true if route is registered otherwise false
+   */
   private function isRouteRegistered(string $endPoint): bool {
     if (!array_key_exists($this->method, $this->routes)) {
       return false;
@@ -26,14 +32,27 @@ class Router {
     return array_key_exists($endPoint, $this->routes[$this->method]);
   }
 
+  /*
+   * Register the get routes
+   * @params $route -> route for which the callback is registered
+   * @params $callback -> the callback function for the route
+   */
   public function get(string $route, Closure $callback): void {
     $this->routes['GET'][$route] = $callback;
   }
 
+  /*
+   * Register the post routes
+   * @params $route -> route for which the callback is registered
+   * @params $callback -> the callback function for the route
+   */
   public function post(string $route, Closure $callback): void {
     $this->routes['POST'][$route] = $callback;
   }
 
+  /*
+   * Checks if the current route is registered with a callback and runs the callback
+   */
   public function run(): void {
     if (!$this->isRouteRegistered($this->endpoint)) {
       return;
